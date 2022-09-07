d672289b03f5e7ea5948757006398c7a04f78e35
marksikaundi <emmanuelmark117@gmail.com>
2022-09-07 14:40:40 -0700

    settings

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <button>Learn React</button>
         </a>
       </header>
     </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+
+function Footer() {
+  return (
+    <div>
+      <h1 className='App'>DevCircle Africa</h1>
+    </div>
+  )
+}
+
+export default Footer
