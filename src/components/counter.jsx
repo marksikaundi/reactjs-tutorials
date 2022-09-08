ba953230c2766908e9a6c4a8b4a18c6bab9bcf96
marksikaundi <emmanuelmark117@gmail.com>
2022-09-08 03:31:28 -0700

    apply class attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,21 +4,19 @@ import React, { Component } from 'react'
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
     };
     render() { 
         return (
             <div>
-                <img src={this.state.imageUrl} alt="" />
-                <span>{this.formatCount()}</span> 
-                <button>increament</button>
+                <span className='badge badge-primary m-2'>{this.formatCount()}</span> 
+                <button className='btn btn-secondary btn-sm'>increament</button>
             </div>
         
         )
     }
 
     formatCount() {
-        const { count } = this.state
+         const { count } = this.state
         return count === 0 ? <h1>Zero</h1> : count;
     }
 } 
