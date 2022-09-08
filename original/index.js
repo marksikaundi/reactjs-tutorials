a6fe6d6e34fae3518c997be8f6afedca4e802219
marksikaundi <emmanuelmark117@gmail.com>
2022-09-08 02:45:01 -0700

    setting counter app

--- original/index.js
+++ original/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
-
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
